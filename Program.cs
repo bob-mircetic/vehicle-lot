4b5939af645ac56c41f12a827b4832bf6b12e37d
Mircetic <17263@nelnet.net>
2021-06-24 15:29:04 -0600

    eod Thurs 6-24

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace vehicle_lot
+{
+    internal class Vehicle {
+
+    }
+
+    class Car : Vehicle {
+
+    } 
+
+    class Semi : Vehicle {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
